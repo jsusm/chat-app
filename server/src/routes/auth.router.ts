29c8b09d5c2ffdaf2a2916ae39406fa83a5078c6
Jesus Marcano <jsusmarcano.jm@gmail.com>
2023-05-24 10:13:30 -0400

    feat: create /auth/auth/signin route

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response, NextFunction } from 'express'
-import { signupSchema } from '../schemas/auth.schema.js'
+import { signinSchema, signupSchema } from '../schemas/auth.schema.js'
 import bcrypt from 'bcrypt'
 import { db } from '../db/db.js'
 import { users } from '../db/schema.js'
 import { SqliteError } from 'better-sqlite3'
 import { signJWT } from '../lib/jwt.js'
+import { eq } from 'drizzle-orm'
 
 const router = Router()
 
@@ -35,6 +36,37 @@ router.post('/signup', async (req: Request, res: Response, next: NextFunction) =
     res.json({
       token,
       user: newUser,
+    })
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+router.post('/signin', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const body = signinSchema.parse(req.body)
+    const [user] = db
+      .select()
+      .from(users)
+      .where(eq(users.name, body.name))
+      .all()
+    if (!user) {
+      res.status(400)
+      res.json({ message: "Username and password don't match" })
+      return
+    }
+    const passwordMatch = await bcrypt.compare(body.password, user.password);
+    if (!passwordMatch) {
+      res.status(400)
+      res.json({ message: "Username and password don't match" })
+      return
+    }
+    const token = await signJWT(user)
+    res.status(200)
+    res.json({
+      token,
+      user: { id: user.id, name: user.name },
     })
   }
   catch (error) {
