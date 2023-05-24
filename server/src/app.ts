9a68c4dc2302c4cfa99f05e6f5d6cebfe86576ab
Jesus Marcano <jsusmarcano.jm@gmail.com>
2023-05-24 09:44:26 -0400

    feat: setup logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import morgan from 'morgan'
 import * as express from 'express'
 import routes from './routes/index.js'
 import { errorMiddleware } from './middlewares/error.middleware.js'
@@ -7,6 +8,7 @@ import { errorMiddleware } from './middlewares/error.middleware.js'
 const app = express.default()
 
 // middlewares
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cors({
   origin: ['http://localhost:1573'],
