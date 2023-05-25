dc01ba3cc38460b37d8cd1608254f5efbb4917f2
Jesus Marcano <jsusmarcano.jm@gmail.com>
2023-05-25 11:27:08 -0400

    feat: create chats and messages database tables

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -9,4 +9,24 @@ export const users = sqliteTable('users', {
   nameIdx: uniqueIndex('nameIdx').on(users.name)
 }))
 
+export const chats = sqliteTable('chats', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+})
+
+export const chatSubs = sqliteTable('chat_subscriptions', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  chatId: integer('chat_id').references(() => chats.id),
+  userId: integer('author_id').references(() => users.id),
+})
+
+export const messages = sqliteTable('messages', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  chatSubId: integer('id').references(() => chatSubs.id),
+  createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+  content: text('content').notNull(),
+})
+
 export type UserDB = InferModel<typeof users>
+export type ChatDB = InferModel<typeof chats>
+export type ChatSub = InferModel<typeof chatSubs>
+export type MessageDB = InferModel<typeof messages>
